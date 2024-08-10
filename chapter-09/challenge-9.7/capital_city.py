ab9a792ed195884d2e30e31ef02ea3c45a2dcc44
zhid0399123 <zhid0399123@gmail.com>
2024-08-10 22:26:22 +0300

    capital city loop

---
+++ chapter-09/challenge-9.7/capital_city.py
@@ -0,0 +1,74 @@
+import random
+
+CAPITALS_DICT = {
+    "Alabama": "Montgomery",
+    "Alaska": "Juneau",
+    "Arizona": "Phoenix",
+    "Arkansas": "Little Rock",
+    "California": "Sacramento",
+    "Colorado": "Denver",
+    "Connecticut": "Hartford",
+    "Delaware": "Dover",
+    "Florida": "Tallahassee",
+    "Georgia": "Atlanta",
+    "Hawaii": "Honolulu",
+    "Idaho": "Boise",
+    "Illinois": "Springfield",
+    "Indiana": "Indianapolis",
+    "Iowa": "Des Moines",
+    "Kansas": "Topeka",
+    "Kentucky": "Frankfort",
+    "Louisiana": "Baton Rouge",
+    "Maine": "Augusta",
+    "Maryland": "Annapolis",
+    "Massachusetts": "Boston",
+    "Michigan": "Lansing",
+    "Minnesota": "Saint Paul",
+    "Mississippi": "Jackson",
+    "Missouri": "Jefferson City",
+    "Montana": "Helena",
+    "Nebraska": "Lincoln",
+    "Nevada": "Carson City",
+    "New Hampshire": "Concord",
+    "New Jersey": "Trenton",
+    "New Mexico": "Santa Fe",
+    "New York": "Albany",
+    "North Carolina": "Raleigh",
+    "North Dakota": "Bismarck",
+    "Ohio": "Columbus",
+    "Oklahoma": "Oklahoma City",
+    "Oregon": "Salem",
+    "Pennsylvania": "Harrisburg",
+    "Rhode Island": "Providence",
+    "South Carolina": "Columbia",
+    "South Dakota": "Pierre",
+    "Tennessee": "Nashville",
+    "Texas": "Austin",
+    "Utah": "Salt Lake City",
+    "Vermont": "Montpelier",
+    "Virginia": "Richmond",
+    "Washington": "Olympia",
+    "West Virginia": "Charleston",
+    "Wisconsin": "Madison",
+    "Wyoming": "Cheyenne",
+}
+
+stateList = list(CAPITALS_DICT.items())
+state, capital = random.choice(stateList)
+
+
+while True:
+    prompt = f"What is the Capital of '{state}'?\n"
+    userInput = input(prompt)
+
+    if userInput.upper() == "EXIT":
+        print(f"The capital of '{state}' is '{capital}'")
+        print("\nGoodbye :/")
+        break
+
+    elif userInput.lower() == capital.lower():
+        print("Correct :/")
+        break
+
+    print("\nIncorrect :/\n")
+    continue
